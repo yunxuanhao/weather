4f93ba1f3e83a0f5b9194f3aa22c4bc63c3b0a10
yunxuan <yx1@meitu.com>
2018-09-05 16:20:16 +0800

    first commit

---
+++ src/Exception/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by yx1@meitu.com
+ * Date: 2018/9/5
+ * Time: 下午2:57
+ */
+
+namespace Ejpill\Weather\Exception;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by yx1@meitu.com
+ * Date: 2018/9/5
+ * Time: 下午2:58
+ */
+
+namespace Ejpill\Weather\Exception;
+
+
+class InvalidArgumentException extends Exception
+{
+
+}

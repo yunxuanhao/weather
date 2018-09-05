53172e8bccd59976d0ffbbb71b7a38d10cfbefcf
yunxuanhao <yunxuanhao@gmail.com>
2018-09-05 09:10:21 +0000

    Apply fixes from StyleCI

--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -1,14 +1,16 @@
 <?php
-/**
- * Created by yx1@meitu.com
- * Date: 2018/9/5
- * Time: 下午2:57
+
+/*
+ * This file is part of the yunxuanhao/weather.
+ *
+ * (c) yunxuanhao <yunxuanhao@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Yunxuanhao\Weather\Exception;
 
-
 class Exception extends \Exception
 {
-
 }
